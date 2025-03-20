9abee43b692279527b0db455b6ebb21ac42d9721
M3RD-Nanda <m.3rd.nanda@gmail.com>
2025-03-20 18:22:41 +0700

    Testing code changes

--- docs/marketplace/products.js
+++ docs/marketplace/products.js
@@ -1,30 +1,32 @@
+// products.js
+
 const productData = [
     {
         id: 1,
-        nama: "Financial Statement Creation Service",
-        harga: 500000,
-        detail: "Comprehensive financial statement creation service, including balance sheets, income statements, and cash flow statements, tailored to your business needs.",
-        gambar: "marketplace/images/financial-statement.jpg"
+        nama: "Coming Soon",
+        harga: 0,
+        detail: "This Digital Product is currently in development. I am dedicated to creating a valuable and innovative product for you.",
+        gambar: "images/Coming-Soon-4.png"
     },
     {
         id: 2,
-        nama: "Graphic Design Service",
-        harga: 300000,
-        detail: "Professional graphic design services for logos, brochures, posters, and social media content. Enhance your brand's visual identity.",
-        gambar: "marketplace/images/graphic-design.jpg"
+        nama: "Coming Soon",
+        harga: 0,
+        detail: "This Digital Product is currently in development. I am dedicated to creating a valuable and innovative product for you.",
+        gambar: "images/Coming-Soon-4.png"
     },
     {
         id: 3,
-        nama: "Business Data Analysis Service",
-        harga: 400000,
-        detail: "In-depth business data analysis services using Excel, SPSS, and other tools to provide actionable insights for your business.",
-        gambar: "marketplace/images/data-analysis.jpg"
+        nama: "Coming Soon",
+        harga: 0,
+        detail: "This Digital Product is currently in development. I am dedicated to creating a valuable and innovative product for you.",
+        gambar: "images/Coming-Soon-4.png"
     },
     {
         id: 4,
-        nama: "Website Development Service",
-        harga: 1000000,
-        detail: "Custom website development service using HTML, CSS, and JavaScript.  Create a stunning and functional online presence for your business.",
-        gambar: "marketplace/images/website-development.jpg"
+        nama: "Coming Soon",
+        harga: 0,
+        detail: "This Digital Product is currently in development. I am dedicated to creating a valuable and innovative product for you.",
+        gambar: "images/Coming-Soon-4.png"
     }
 ];
